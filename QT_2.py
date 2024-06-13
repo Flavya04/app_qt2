397f873219acc4271d1fab9e58263f019d128689
Flavya04 <flavya2310@gmail.com>
2024-06-13 13:42:25 -0300

    teste

--- QT_2.py
+++ QT_2.py
@@ -38,3 +38,12 @@ introducao(selected, menu)
 teste(selected, menu)
 modelagem(selected, menu)
 calibragem(selected, menu)
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
