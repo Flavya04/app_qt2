020431570aac742c05d29641e932aadfad83c005
Flavya Vilela <94541367+Flavya04@users.noreply.github.com>
2024-06-14 10:49:18 -0300

    Update QT_2.py

--- QT_2.py
+++ QT_2.py
@@ -8,15 +8,6 @@ from instrucoes import teste
 import streamlit as st
 
 from PIL import Image
-# page_bg_img = """
-# <style>
-# [data-testid="stAppViewContainer"] {
-# background-color: #212750;
-# }
-# </style>
-# """
-# st.markdown(page_bg_img, unsafe_allow_html=True)
-#opening the image
 
 image = Image.open('LOGO.png')
 
